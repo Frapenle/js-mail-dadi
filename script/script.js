e20f299cc1c37993e8f22128c80c21155c7aa3b7
francescotrudu <francescotrudu84@gmail.com>
2022-11-24 19:42:04 +0100

    dadi game

--- script/script.js
+++ script/script.js
@@ -1,1 +1,31 @@
-console.log("hello world!")
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const buttonElement = document.getElementById("randomButton");
+
+buttonElement.addEventListener("click", function(){
+    const userOneValue = document.getElementById("user").value;
+    const playerOne = Math.floor(Math.random() * 6) + 1;
+    const playerTwo = Math.floor(Math.random() * 6) + 1;
+    const pcResultText = document.getElementById("pc");
+    const userResultText = document.getElementById("player");
+    const result = document.querySelector(".result");
+    
+    
+    if (playerOne > playerTwo) {
+        userResultText.innerHTML = `<p class="output">${userOneValue} hai totalizzato ${playerOne}</p>`;
+        pcResultText.innerHTML = `<p class="output">PC ha totalizzato ${playerTwo}</p>`;
+        result.innerHTML = ("HAI VINTO");
+    } else if (playerOne < playerTwo) {
+        userResultText.innerHTML = `<p class="output">${userOneValue} hai totalizzato ${playerOne}</p>`;
+        pcResultText.innerHTML = `<p class="output">PC ha totalizzato ${playerTwo}</p>`;
+        result.innerHTML = ("HAI PERSO");
+
+    } else {
+        userResultText.innerHTML = `<p class="output">${userOneValue} hai totalizzato ${playerOne}</p>`;
+        pcResultText.innerHTML = `<p class="output">PC ha totalizzato ${playerTwo}</p>`;
+        result.innerHTML = ("PARITÀ");
+    }
+})
+
